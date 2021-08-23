0a6492bd40f5738e9671280a8107a16ba8e295db
unknown <joumana.m.moussa@gmail.com>
2021-08-23 14:56:47 +0300

    Test commit

--- JoumanaMoussa-Ex6.php
+++ JoumanaMoussa-Ex6.php
@@ -4,7 +4,7 @@ function binToDec($x) {                     //turning the variable to an array s
     $newx = str_split($x);
     $newx = array_reverse($newx);           //reversing the array so we can use the index as the power value
     $dec = 0;
-    // print_r($newx);
+    print_r($newx);
 
     foreach($newx as $index => $elmt) {
         if ($elmt == 1) {
